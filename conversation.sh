15e6b9b6af77c6435c0138505155228c1dab5714
sravani-506 <bellagubbalasravani@gmail.com>
2025-05-22 17:03:30 +0530

    hello world started

---
+++ conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Riya: hey Sri, How are you?"
+echo "Sri: Hii riay, I'm fine"
+echo "Riya: What are you doing?"
+echo "Sri: I'm eating 5star"
+echo "Riya: Ok, eat 5star and do nothing"
