fa4cb9543e994695682def71b3c9a3fc2bb707f5
sravani-506 <bellagubbalasravani@gmail.com>
2025-05-22 17:11:59 +0530

    hello world started

--- variables.sh
+++ variables.sh
@@ -2,7 +2,7 @@
 P1=Riya
 P2=Sri
 
-echo "$P1: hey $$P2, How are you?"
+echo "$P1: hey $P2, How are you?"
 echo "$P2: Hii $P1, I'm fine"
 echo "$P1: What are you doing?"
 echo "$P2: I'm eating 5star"
