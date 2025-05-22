04c20ab34e7dc141c93ccabae34a39d84ba2b771
sravani-506 <bellagubbalasravani@gmail.com>
2025-05-22 17:28:28 +0530

    hello world started

---
+++ 04-variableArg.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+P1=$1
+P2=$2
+
+echo "$P1: hey $P2, How are you?"
+echo "$P2: Hii $P1, I'm fine"
+echo "$P1: What are you doing?"
+echo "$P2: I'm eating 5star"
+echo "$P1: Ok, eat 5star and do nothing"
--- hello.py
+++
@@ -1,1 +0,0 @@
-sdzsdcfzsvcdzcz
