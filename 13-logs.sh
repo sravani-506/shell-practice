55b458ed1f240ea6b1bbc4abf594ad870d007a2d
sravani-506 <bellagubbalasravani@gmail.com>
2025-05-23 19:26:49 +0530

    hello world started

--- 13-logs.sh
+++ 13-logs.sh
@@ -33,28 +33,28 @@ VALIDATE(){
 }
 
 
-dnf list installed mysql &>> $log_file
+dnf list installed mysql &>>$log_file
 
 #check already installed or not. If installed $? value is 0, then
 # If not installed $? is not 0. expresion is true
 if [ $? -ne 0 ]
 then
     echo "MySQL is not installed.. going to install it" | tee -a $log_file
-     dnf install mysql -y
+     dnf install mysql -y &>>$log_file
      VALIDATE $? "MySQL"
 else 
     echo -e "$y MySQL is already installed $N... Nothing to do" | tee -a $log_file
 fi
 
 
-dnf list installed nginx &>> $log_file
+dnf list installed nginx &>>$log_file
 
 #check already installed or not. If installed $? value is 0, then
 # If not installed $? is not 0. expresion is true
 if [ $? -ne 0 ]
 then
     echo "nginx is not installed.. going to install it" | tee -a $log_file
-     dnf install nginx -y &>> $log_file
+     dnf install nginx -y &>>$log_file
      VALIDATE $? "nginx"
 else 
     echo -e "$y nginx is already installed $N... Nothing to do" | tee -a $log_file
